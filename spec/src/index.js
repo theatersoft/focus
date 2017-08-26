d9e46e1ce99730142eacbe417a75a694381600b0
Rob Huehn <rhuehn@gmail.com>
2017-08-25 22:57:59 -0700

    rm unused spec

--- spec/src/index.js
+++
@@ -1,8 +0,0 @@
-import {h, render} from 'preact'
-import './resize'
-import App from './App'
-import './index.styl'
-import '../res/_icons.svg'
-import '@theatersoft/components/components.css'
-
-render(<App />, document.getElementById('ui'))
